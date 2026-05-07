2a9e312500c4b35e5e5af1962c6a607aabc2dbff
iamnobita206243 <nobitamusic001@gmail.com>
2026-05-08 00:41:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -73,9 +73,9 @@ GIT_TOKEN = getenv(
 
 #------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NOB1TA_SUPPORT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+E7jXcV6jpK0xMGJl")
-CHAT = getenv("CHAT", "https://t.me/+DRlOUMim-SdjOWJl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NOBITA_SUPP0RT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+S0Q1-J_EQLA3YmU1")
+CHAT = getenv("CHAT", "https://t.me/+S0Q1-J_EQLA3YmU1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
